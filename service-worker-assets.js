bc8ac546e0ed4b6fa863111c1bc8925f59b9d1e0
Sajad Afaghiy <sajadafaghiy@gmail.com>
2023-12-15 18:58:11 +0330

    service-worker-assets

---
+++ service-worker-assets.js
@@ -0,0 +1,38 @@
+self.assetsManifest = {
+  "version": "v2",
+  "assets": [
+    {
+      "url": "favicon.ico"
+    },
+    {
+      "url": "index.html"
+    },
+    {
+      "url": "manifest.webmanifest"
+    },
+    {
+      "url": "dist/css/site.css"
+    },
+    {
+      "url": "assets/fonts/fonts.css"
+    },
+    {
+      "url": "assets/fonts/fontawesome/css/all.min.css"
+    },
+    {
+      "url": "libs/bootstrap/dist/css/bootstrap.rtl.min.css"
+    },
+    {
+      "url": "libs/bootstrap/dist/css/bootstrap.rtl.min.css.map"
+    },
+    {
+      "url": "assets/icons/favicon-192x192.png"
+    },
+    {
+      "url": "assets/icons/favicon-512x512.png"
+    },
+    {
+      "url": "assets/images/sajad-afaghiy.webp"
+    },
+  ]
+};
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
+self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
-// const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const cacheName = `${cacheNamePrefix}v1`;
-const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
-const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const offlineAssetsInclude = [/\.html$/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
 
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
 const base = "/";
@@ -20,7 +20,7 @@ async function onInstall(event) {
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+        .map(asset => new Request(asset.url, { cache: 'no-cache' }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
